7e3fe40118de9b958ed7a844fc7112ba18eddc8c
Oleksiy Protas <elfy.ua@gmail.com>
2016-10-10 23:32:28 +0300

    Dummy frontend and backend

--- sample/backend/time_teller.py
+++ sample/backend/time_teller.py
@@ -8,8 +8,9 @@ class MyServer(BaseHTTPRequestHandler):
     print("Got a request, serving.")
     self.send_response(200)
     self.send_header("Content-type", "application/json")
+    self.send_header("Access-Control-Allow-Origin", "*")
     self.end_headers()
-    self.wfile.write(bytes(strftime("{ 'time': '%a, %d %b %Y %H:%M:%S +0000' }", gmtime()), "utf8"))
+    self.wfile.write(bytes(strftime('{ "time": "%a, %d %b %Y %H:%M:%S +0000" }', gmtime()), "utf8"))
     return 
   
 httpd = HTTPServer( ( "0.0.0.0", 8080 ), MyServer)
