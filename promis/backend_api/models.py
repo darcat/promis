fa6fbd1466d9ddf16fb282ec375ae2df8982730f
Rostyslav Golda <tx_hv@ukr.net>
2016-10-13 08:10:01 +0300

    Completed with database descriptions. Not tested yet

--- promis/backend_api/models.py
+++ promis/backend_api/models.py
@@ -2,9 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.db.models.fields import DateTimeField, IntegerField, CharField,\
-    DateField
+    DateField, FloatField
 from django.db.models.fields.related import ForeignKey
-
+from jsonfield import JSONField
 
 # Create your models here.
 
@@ -48,8 +48,28 @@ class Values(models.Model):
     description = ForeignKey('Translations')
     short_name = CharField(max_length=100)
     units = ForeignKey('Units')
+
+class Parameters(models.Model):
+    name = ForeignKey('Translations')
+    description = ForeignKey('Translations')
+    value = ForeignKey('Values')
+    conversion = ForeignKey('Functions')
+    conversion_params = TextField()
+    channel = ForeignKey('Channels')
+    quicklook = ForeignKey('Functions')
+
+class Documents(models.Model):
+    last_mod = DateTimeField(auto_now_add = True)
+    json_data = JSONField()
     
-    
-    
+class Measurements(models.Model):
+    session = ForeignKey('Sessions')
+    parameter = ForeignKey('Parameters')
+    channel = ForeignKey('Channels')
+    chn_doc_id = ForeignKey('Documents')
+    par_doc_id = ForeignKey('Documents')
+    sampling_frequency = FloatField()
+    max_frequency  = FloatField()
+    min_frequency  = FloatField()
     
     
