849b1edf7744d900805d0146e6ac7ca19dc350db
Rostyslav Golda <tx_hv@ukr.net>
2016-10-16 01:22:18 +0300

    There is going some sr%%%@@-doshka

--- promis/promis/settings.py
+++ promis/promis/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'promis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'promisdb',
+        'USER': 'promis',
+        'PASSWORD': 'swordfish',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
