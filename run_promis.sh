d53b2335ac0b0897dbad47b2442630625d5e93bd
Oleksiy Protas <elfy.ua@gmail.com>
2016-10-20 03:13:14 +0300

    Graph_models extension

--- run_promis.sh
+++ run_promis.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Migrate data if needed
 python ./promis/manage.py makemigrations
 python ./promis/manage.py migrantion
+# Generate a diagram for the frontend
+#python ./promis/manage.py graph_models -a > my_project.dot
+# Run the actual server
 python ./promis/manage.py runserver 0.0.0.0:80
