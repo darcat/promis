df6d10c88cab87975a508a867fba36b2ffa76b4a
Oleksiy Protas <elfy.ua@gmail.com>
2017-02-02 20:35:06 +0100

    Obvious typo fixed

--- run_promis.sh
+++ run_promis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Migrate data if needed
 python ./promis/manage.py makemigrations backend_api
-python ./promis/manage.py migration
+python ./promis/manage.py migrate
 # Generate a diagram for the frontend
 python ./promis/manage.py graph_models -a > $SYNC_DIR/model.dot
 # Run the actual server
