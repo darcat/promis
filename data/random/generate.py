e570a4c62548580a8b100c28726ee8a54f651ae1
Oleksiy Protas <elfy.ua@gmail.com>
2017-02-03 01:33:55 +0100

    Just realised meridians go both ways xD

--- data/random/generate.py
+++ data/random/generate.py
@@ -20,25 +20,29 @@ def cord_conv(x,y):
 
 # TODO: aspect ratio maaaybe?
 
+### Satellite "Love&Peace" somehow flies an orbit that is projected as a heart and pacific sign
+
 # Heart shape
 def heart():
-    return [ cord_conv(45+2*16*sin(t)**3, 3*13*cos(t)-3*5*cos(2*t)-3*2*cos(3*t)-3*cos(4*t))
+    return [ cord_conv(-4*16*sin(t)**3, 3*13*cos(t)-3*5*cos(2*t)-3*2*cos(3*t)-3*cos(4*t))
         for t in ((l-heart_pts/2)/(heart_pts*0.165)
             for l in range(heart_pts))]
 # Circle
 def circle():
-    return [ cord_conv(90+45+30*cos(t), 30*sin(t))
+    return [ cord_conv(90+60*cos(t), 30*sin(t))
         for t in ((2*pi*l/peace_pts)
             for l in range(peace_pts))]
 
 # Vertical line [-35;35]
 def vline():
-    return [ coord_conv(90+45, t*35)
+    return [ coord_conv(90, t*35)
         for t in (((l-lines_pts/2)/lines_pts)
             for l in range(lines_pts))]
 
 # Upwards tick
 def uptick():
-    return [ coord_conv(90+45+t*30,-10-abs(t*30))
+    return [ coord_conv(90+t*60,-10-abs(t*30))
         for t in (((l-lines_pts/2)/lines_pts)
             for l in range(lines_pts))]]
+
+### Satellite "Roundabout" starts at equator on longitude 0 and flies towards the North Pole with an angle of 1° to the meridian
