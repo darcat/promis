14642f906744074475925da238eeacae9545f97e
Oleksiy Protas <elfy.ua@gmail.com>
2017-02-03 01:28:37 +0100

    Other intriguing shapes

--- data/random/generate.py
+++ data/random/generate.py
@@ -5,6 +5,7 @@ from math import pi, atan, exp, sin, cos
 # Call the cops, I don't care
 heart_pts = 10000
 peace_pts = 10000
+lines_pts = 2000
 orbit_pts = 10000
 
 # Points per orbit segment
@@ -15,13 +16,29 @@ orbit_sec = 20
 
 # Fake mercator considering Earth is a sphere
 def cord_conv(x,y):
-    return float(x),180.0/pi*(2.0*atan(exp(y*pi/180.0))-pi/2.0)
+    return [ float(x),180.0/pi*(2.0*atan(exp(y*pi/180.0))-pi/2.0) ]
+
+# TODO: aspect ratio maaaybe?
 
 # Heart shape
 def heart():
-    return [ [ 45+2*16*sin(t)**3, 18*13*cos(t)-18*5*cos(2*t)-18*2*cos(3*t)-18*cos(4*t) ]
+    return [ cord_conv(45+2*16*sin(t)**3, 3*13*cos(t)-3*5*cos(2*t)-3*2*cos(3*t)-3*cos(4*t))
         for t in ((l-heart_pts/2)/(heart_pts*0.165)
             for l in range(heart_pts))]
+# Circle
+def circle():
+    return [ cord_conv(90+45+30*cos(t), 30*sin(t))
+        for t in ((2*pi*l/peace_pts)
+            for l in range(peace_pts))]
 
-#for i in heart():
-#    print(str(i[0])+","+str(i[1]))
+# Vertical line [-35;35]
+def vline():
+    return [ coord_conv(90+45, t*35)
+        for t in (((l-lines_pts/2)/lines_pts)
+            for l in range(lines_pts))]
+
+# Upwards tick
+def uptick():
+    return [ coord_conv(90+45+t*30,-10-abs(t*30))
+        for t in (((l-lines_pts/2)/lines_pts)
+            for l in range(lines_pts))]]
