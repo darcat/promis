11ac7e83a1f1aaff92a515aca616ab93a074c737
elpiankova <el.piankova@gmail.com>
2017-03-16 18:03:41 +0200

    Added Potential initial data. Fixes #33

--- run_promis.sh
+++ run_promis.sh
@@ -11,6 +11,9 @@ python $PROMIS_DIR/promis/manage.py collect_functions
 # Create superuser if needed
 python $PROMIS_DIR/promis/manage.py batch_create_superuser
 
+# Add initial Potential data
+python $PROMIS_DIR/promis/manage.py loaddata init_data.json
+
 # Generate a diagram for the frontend
 python $PROMIS_DIR/promis/manage.py graph_models -a > $SYNC_DIR/model.dot
 # Run the actual server
