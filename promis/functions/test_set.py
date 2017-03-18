edafc3c4d0d762d2a0a7554c228639e0e40b5c19
Oleksiy Protas <elfy.ua@gmail.com>
2017-03-18 14:43:11 +0200

    Functions for test_set data

---
+++ promis/functions/test_set.py
@@ -0,0 +1,27 @@
+#
+# Copyright 2016 Space Research Institute of NASU and SSAU (Ukraine)
+#
+# Licensed under the EUPL, Version 1.1 or – as soon they
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/software/page/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+#
+
+def data_func(satellite_object):
+    def check():
+        pass
+    
+    def fetch(data_id):
+        pass
