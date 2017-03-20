3522213a2f4af4f28e1085a6b9d824f42ba3211d
Oleksiy Protas <elfy.ua@gmail.com>
2017-03-21 00:51:13 +0200

    Correct path

--- tests_backend/test_data_check.py
+++ tests_backend/test_data_check.py
@@ -3,8 +3,8 @@ import subprocess
 
 @pytest.fixture
 def data_fetch():
-    return subprocess.run("./repos/promis-testing/api_command check_data_updates > /tmp/check_data_updates.log", shell=True)
+    return subprocess.run("./api_command check_data_updates > /tmp/check_data_updates.log", shell=True)
     # TODO: return something more meaningful?
 
 def test_data_fetch(data_fetch):
-    assert data_fetch.returnvalue == 0
+    assert data_fetch.returncode == 0
