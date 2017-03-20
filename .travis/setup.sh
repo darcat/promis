66ddf37c3aad8b4896f41762c74e9546efbbf512
Oleksiy Protas <elfy.ua@gmail.com>
2017-03-21 00:36:51 +0200

    Python-3-isation
    Fixes #40

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -6,7 +6,7 @@ sh -e /etc/init.d/xvfb start
 # FTP server
 mkdir ftproot
 cd ftproot
-/usr/bin/python2.7 -m pyftpdlib > /tmp/ftp.log 2>&1 &
+/usr/bin/python3 -m pyftpdlib > /tmp/ftp.log 2>&1 &
 echo "=> FTP service started"
 
 # Generate data for FTP
