1dbbeaddfad2f94d64c950718839d764d77c0219
Oleksiy Protas <elfy.ua@gmail.com>
2017-03-21 13:19:48 +0200

    api ⇒ backend renaming

--- tests_backend/test_data_check.py
+++ tests_backend/test_data_check.py
@@ -3,7 +3,7 @@ import subprocess
 
 @pytest.fixture
 def data_fetch():
-    return subprocess.run("./api_command check_data_updates > /tmp/check_data_updates.log", shell=True)
+    return subprocess.run("./backend_command check_data_updates > /tmp/check_data_updates.log", shell=True)
     # TODO: return something more meaningful?
 
 def test_data_fetch(data_fetch):
