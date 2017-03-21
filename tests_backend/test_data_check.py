2d7bdd9359458ee8f83ef74a5e0b64daf8494b7c
Oleksiy Protas <elfy.ua@gmail.com>
2017-03-21 13:46:43 +0200

    Fix for Python < 3.5

--- tests_backend/test_data_check.py
+++ tests_backend/test_data_check.py
@@ -1,10 +1,17 @@
 import pytest
 import subprocess
+import sys
 
 @pytest.fixture
 def data_fetch():
-    return subprocess.run("./backend_command check_data_updates > /tmp/check_data_updates.log", shell=True)
+    cmd = "./backend_command check_data_updates > /tmp/check_data_updates.log"
+
+    # Call semantics changed in 3.5
+    if sys.version_info < (3,5):
+        return subprocess.call(cmd, shell=True)
+    else:
+        return subprocess.run(cmd, shell=True).returncode
     # TODO: return something more meaningful?
 
 def test_data_fetch(data_fetch):
-    assert data_fetch.returncode == 0
+    assert data_fetch == 0
