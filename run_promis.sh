9bbb167463063d91c865a97860dab638b8316dd7
Oleksiy Protas <elfy.ua@gmail.com>
2017-03-22 05:16:04 +0200

    Opting to use django-extensions' profilers
    Provides tools to fix #58
    
    Workaround implementation, see django-extensions/django-extensions#1035

--- run_promis.sh
+++ run_promis.sh
@@ -14,5 +14,21 @@ python $PROMIS_DIR/promis/manage.py graph_models -a > $SYNC_DIR/model.dot
 # Generate static assets
 python $PROMIS_DIR/promis/manage.py collectstatic --no-input
 
+# Enable proflinig if debug is enabled
+if [ "$DJANGO_DEBUG" = "true" ]; then
+    if [ ! -d $SYNC_DIR/profiling ]; then
+        echo "=> Creating sync/profiling directory."
+        mkdir $SYNC_DIR/profiling
+    else
+        echo "=> sync/profiling directory exists."
+    fi
+    SERVER_COMMAND="runprofileserver --use-cprofile --kcachegrind --nostatic --prof-path $SYNC_DIR/profiling"
+    
+    # TODO: remove after upstream django-extensions merge #1035
+    (cd /usr/local/lib/python3.6/site-packages/django_extensions/management/commands/ && patch -p0) < fix_runprofileserver_ipport.patch
+else
+    SERVER_COMMAND="runserver"
+fi
+
 # Run the actual server
-python $PROMIS_DIR/promis/manage.py runserver 0.0.0.0:80
+python $PROMIS_DIR/promis/manage.py $SERVER_COMMAND 0.0.0.0:80
