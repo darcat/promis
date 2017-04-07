447317b31cfa6eeb7f7f44161e5a8829e77ea8b4
Oleksiy Protas <elfy.ua@gmail.com>
2017-04-07 23:43:29 +0300

    Entry point for API tests
    Fixes #12

--- tests_backend/test_1_auth.py
+++ tests_backend/test_1_auth.py
@@ -10,6 +10,7 @@ def test_wrong_password(session):
     '''Check that we can't log in with wrong credentials'''
     assert not session.login("promis", "sunfish"), "Wrong credentials work"
 
+# TODO: this behaviour may and will change!
 def test_unauth_access(session):
     '''Check that we can't access the API without authenticating first'''
     assert 400 <= session.get("http://localhost:8081/en/api/").status_code < 500, "Unauthenticated access possible"
@@ -22,22 +23,42 @@ def superuser(session):
 # TODO: replace with test data instead
 def test_potential_data(superuser):
     '''Check that Potential is correctly inserted'''
-    json_data = superuser.get("http://localhost:8081/en/api/projects/1").json()
+    r = superuser.get("http://localhost:8081/en/api/projects/1")
+    assert r.status_code == 200, "Invalid status code"
+    json_data = r.json()
     assert "name" in json_data, "Invalid JSON recieved"
     assert json_data["name"] == "Potential", "Project #1 is not named Potential"
 
 # 3 users with different access levels
 @pytest.fixture
 def john(session):
-    session.login("john","test")
+    session.login("john", "test")
     return session
 
 @pytest.fixture
 def melanie(session):
-    session.login("melanie","test")
+    session.login("melanie", "test")
     return session
 
 @pytest.fixture
 def connie(session):
-    session.login("connie","test")
+    session.login("connie", "test")
     return session
+
+@pytest.mark.xfail
+def test_level2_access(melanie):
+    '''Check that level2 users can see only parameter fields'''
+    r = superuser.get("http://localhost:8081/en/api/download/1")
+    assert r.status_code == 200, "Invalid status code"
+    json_data = r.json()
+    assert "chn_doc" not in json_data, "Can see channels"
+    assert "par_doc" in json_data, "Can't see parameters"
+
+@pytest.mark.xfail
+def test_level1_access(connie):
+    '''Check that level1 users can see all fields'''
+    r = superuser.get("http://localhost:8081/en/api/download/1")
+    assert r.status_code == 200, "Invalid status code"
+    json_data = r.json()
+    assert "chn_doc" in json_data, "Can't see channels"
+    assert "par_doc" in json_data, "Can't see parameters"
