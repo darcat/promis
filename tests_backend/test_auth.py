028c18528c96773500a456409e0cce5796a589a3
Oleksiy Protas <elfy.ua@gmail.com>
2017-04-07 21:43:45 +0300

    Test users
    Password: test

--- tests_backend/test_auth.py
+++ tests_backend/test_auth.py
@@ -12,4 +12,32 @@ def test_wrong_password(session):
 
 def test_unauth_access(session):
     '''Check that we can't access the API without authenticating first'''
-    assert 400 <= session.get("http://localhost:8081/api").status_code < 500, "Unauthenticated access possible"
+    assert 400 <= session.get("http://localhost:8081/en/api/").status_code < 500, "Unauthenticated access possible"
+
+@pytest.fixture
+def superuser(session):
+    session.login("promis", "swordfish")
+    return session
+
+# TODO: replace with test data instead
+def test_potential_data(superuser):
+    '''Check that Potential is correctly inserted'''
+    json_data = superuser.get("http://localhost:8081/en/api/projects/1").json()
+    assert "name" in json_data, "Invalid JSON recieved"
+    assert json_data["name"] == "Potential", "Project #1 is not named Potential"
+
+# 3 users with different access levels
+@pytest.fixture
+def john(session):
+    session.login("john","test")
+    return session
+
+@pytest.fixture
+def melanie(session):
+    session.login("melanie","test")
+    return session
+
+@pytest.fixture
+def connie(session):
+    session.login("connie","test")
+    return session
