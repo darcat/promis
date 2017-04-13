af56676874026eb1779acd036089ec17a2f1896c
Oleksiy Protas <elfy.ua@gmail.com>
2017-04-13 22:19:04 +0300

    Testing code now translates loopback address to docker one

--- test/tests_backend/auth.py
+++ test/tests_backend/auth.py
@@ -7,7 +7,14 @@ def _get_origin():
     # TODO: disover path on our own
     # TODO: discuss whether we need YML at all?
     with open("deploy/promis_api.yaml") as fp:
-        return yaml.load(fp)["host"]
+        origin =  yaml.load(fp)["host"].split(":")
+
+        # Translate loopback address to docker inter-container interface
+        if origin[0] == "localhost" or origin[0] == "127.0.0.1":
+            origin[0] = "172.17.0.1"
+        if len(origin) > 1:
+            origin[0] += ":" + origin[1]
+        return origin[0]
 
 class Session:
     '''Temporary class to envelop a session'''
