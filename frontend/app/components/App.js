48bb59d2733d513f20dd5238c6e98dee80a10e85
Alex Revin <lyssdod@gmail.com>
2017-04-17 07:55:00 +0300

    Panels test

--- frontend/app/components/App.js
+++ frontend/app/components/App.js
@@ -13,92 +13,18 @@ class App extends React.Component {
         return (
             <div>
                 <Nav />
-                <div>
+                <div style = {{ width: '95%', margin: '0 auto' }}>
                     <Well bsSize="large">
                         <h3>Ionosat PROMIS</h3>
                         <p>We are glad to welcome you on this page. Please use the filters below to refine your search</p>
                     </Well>
                     <Row>
-                        <Panel>
-                            <form class="form-horizontal">
-                              <div class="form-group">
-                                <div class = 'col-sm-2'>
-                                  <label for="daterange" class="control-label">Interval</label>
-                                </div>
-                                <div class = 'col-sm-10'>
-                                  <input type="text" class="form-control daterange" name="daterange" value="08/01/2011 - 09/30/2011" />
-                                </div>
-                              </div>
-                              <div class="form-group">
-                                <div class = 'col-sm-2'>
-                                  <label class = 'control-label'>Input</label>
-                                </div>
-                                <div class = 'col-sm-10'>
-                                  <input type="checkbox" data-toggle="toggle" class = 'loctoggler'
-                                    data-on="<i class='glyphicon glyphicon-screenshot'></i> Use map" data-off="<i class='glyphicon glyphicon-list-alt'></i> Manual" />
-                                </div>
-                              </div>
-                              <div class="form-group textlocation">
-                                <div class = 'col-sm-2'>
-                                  <label for="geoalt" class="control-label">Altitude</label>
-                                </div>
-                                <div class="col-sm-5">
-                                  <div class = 'input-group'>
-                                    <span class="input-group-addon" id="basic-addon1">From</span>
-                                    <input class="form-control" type="number" aria-describedby="basic-addon1" value="400" id="geoalt1" />
-                                  </div>
-                                </div>
-                                <div class="col-sm-5">
-                                  <div class = 'input-group'>
-                                    <span class='input-group-addon' id='basic-addon2'>To</span>
-                                    <input class="form-control" type="number" aria-describedby="basic-addon2" value="440" id="geoalt2" />
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="form-group textlocation">
-                                <div class = 'col-sm-2'>
-                                  <label for="geolat" class="control-label">Latitude</label>
-                                </div>
-                                <div class="col-sm-5">
-                                  <div class = 'input-group'>
-                                    <span class="input-group-addon" id="basic-addon3">From</span>
-                                    <input class="form-control" type="number" aria-describedby="basic-addon3" value="8.407168" id="geolat1" />
-                                  </div>
-                                </div>
-                                <div class="col-sm-5">
-                                  <div class = 'input-group'>
-                                    <span class='input-group-addon' id='basic-addon4'>To</span>
-                                    <input class="form-control" type="number" aria-describedby="basic-addon4" value="5.441022" id="geolat2" />
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="form-group textlocation">
-                                <div class = 'col-sm-2'>
-                                  <label for="geolon" class="control-label">Longtitude</label>
-                                </div>
-                                <div class="col-sm-5">
-                                  <div class = 'input-group'>
-                                    <span class="input-group-addon" id="basic-addon5">From</span>
-                                    <input class="form-control" type="number" aria-describedby="basic-addon5" value="-65.214844" id="geolon1" />
-                                  </div>
-                                </div>
-                                <div class="col-sm-5">
-                                  <div class = 'input-group'>
-                                    <span class='input-group-addon' id='basic-addon6'>To</span>
-                                    <input class="form-control" type="number" aria-describedby="basic-addon6" value="-61.171875" id="geolon2" />
-                                  </div>
-                                </div>
-                              </div>
-                              <div class='form-group maplocation'>
-                                <div class = 'col-sm-2'>
-                                  <label class = 'control-label'>Selection</label>
-                                </div>
-                                <div class = 'col-sm-10'>
-                                  <ul class = 'curselect'></ul>
-                                </div>
-                              </div>
-                            </form>
-                        </Panel>
+                        <Panel>Panel one</Panel>
+                        <Panel>Panel two</Panel>
+                    </Row>
+                    <Row>
+                        <Panel>Panel three</Panel>
+                        <Panel>Panel four</Panel>
                     </Row>
                 </div>
             </div>
