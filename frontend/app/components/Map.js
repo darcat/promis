80d37169e70033c9d1db4a6f2bb7698b4df79fad
Alex Revin <lyssdod@gmail.com>
2017-04-19 22:31:36 +0300

    Zoombox

--- frontend/app/components/Map.js
+++ frontend/app/components/Map.js
@@ -18,7 +18,7 @@ class ToolboxButton extends React.Component {
 
     render() {
         return (
-            <OverlayTrigger placement="bottom" overlay={this.tooltip}>
+            <OverlayTrigger placement = {this.props.placement} overlay = {this.tooltip}>
                 <Button onClick = {this.props.onClick} bsStyle = 'default'>
                     <Glyphicon glyph = {this.props.icon} />
                 </Button>
@@ -30,6 +30,7 @@ class ToolboxButton extends React.Component {
 ToolboxButton.defaultProps = {
     help : 'Tooltip text',
     icon : 'star',
+    placement: 'bottom',
     onClick : function() { ; }
 }
 
@@ -50,7 +51,7 @@ class MapToolbox extends React.Component {
                     <ToolboxButton icon = 'globe' help = 'Switch to 3D' />
                     <ToolboxButton icon = 'edit' help = 'Select area' />
                     <ToolboxButton icon = 'screenshot' help = 'Select area' />
-                    <ToolboxButton icon = 'th' help = 'Select area' />
+                    <ToolboxButton icon = 'th' help = 'Toggle grid' />
                     <ToolboxButton icon = 'resize-full' help = 'Fullscreen' />
                     <ToolboxButton icon = 'erase' help = 'Erase last selection' />
                     <ToolboxButton icon = 'ban-circle' help = 'Clear all selection' />
@@ -63,18 +64,58 @@ class MapToolbox extends React.Component {
 class MapZoomBox extends React.Component {
     constructor(props) {
         super(props);
+
+        this.minZoom = props.minZoom;
+        this.maxZoom = props.maxZoom;
+
+        this.state = {
+            zoom: props.defaultZoom
+        }
+
+        this.zoomIn = this.zoomIn.bind(this);
+        this.zoomOut = this.zoomOut.bind(this);
+    }
+
+    zoomIn() {
+        this.setState(function() {
+            return {
+                zoom: (this.state.zoom < this.maxZoom ? this.state.zoom + 1 : this.maxZoom)
+            }
+        })
+    }
+
+    zoomOut() {
+        this.setState(function() {
+            return {
+                zoom: (this.state.zoom > this.minZoom ? this.state.zoom - 1 : this.minZoom)
+            }
+        })
     }
 
     render() {
         return (
             <div className = 'mapzoombox'>
                 <ButtonGroup vertical>
-                    <ToolboxButton icon = 'plus' help = 'Zoom in' />
-                    <ToolboxButton icon = 'minus' help = 'Zoom out' />
+                    <ToolboxButton placement = 'right' onClick = {this.zoomIn} icon = 'plus' help = 'Zoom in' />
+                    <ToolboxButton placement = 'right' onClick = {this.zoomOut} icon = 'minus' help = 'Zoom out' />
                 </ButtonGroup>
             </div>
         )
     }
+}
+
+MapZoomBox.defaultProps = {
+    minZoom: 1,
+    maxZoom: 15,
+    defaultZoom: 5
+}
+
+function LeafletContainer(props) {
+    return (<div>hello im leaflet</div>)
+}
+
+function CesiumContainer(props) {
+    return (<div>hello im cesium</div>)
 }
 
 class UniversalMap extends React.Component {
@@ -82,23 +123,44 @@ class UniversalMap extends React.Component {
         super(props);
 
         this.state = {
-            flat: true
+            flat: true,      /* true for leaflet, false for cesium */
+            selection: [],    /* active selection */
+            location: { /* current location */
+                lat: null,
+                lng: null,
+                zoom: 5
+            }
         }
 
-        this.go = new GeoObject();
+        //this.go = new GeoObject();
         //this.go.init('#');
+        this.updateMap = this.updateMap.bind(this);
+        this.handleZoom = this.handleZoom.bind(this);
+        this.handleTools = this.handleTools.bind(this);
+    }
+
+    updateMap() {
+
+    }
+
+    handleZoom() {
+
+    }
+
+    handleTools() {
+
     }
 
     render() {
         return (
             <div>
-                <MapZoomBox />
-                <MapToolbox />
+                <MapZoomBox onChange = {this.handleZoom} />
+                <MapToolbox onChange = {this.handleTools} />
 
                 { this.state.flat ? (
-                <div id = 'leaflet'></div>
+                <LeafletContainer onChange = {this.updateMap} location = {this.state.location} selection = {this.state.selection} />
                 ) : (
-                <div id = 'cesium'></div>
+                <CesiumContainer onChange = {this.updateMap} location = {this.state.location} selection = {this.state.selection} />
                 ) }
             </div>
         );
