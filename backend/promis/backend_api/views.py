b960483eaafd73e7a9bfd49db458f01ddd9091b8
Oleksiy Protas <elfy.ua@gmail.com>
2017-04-24 11:32:07 +0300

    JSON data saving

--- backend/promis/backend_api/views.py
+++ backend/promis/backend_api/views.py
@@ -37,7 +37,7 @@ class SessionFilter(django_filters.rest_framework.FilterSet):
                                                queryset = models.Space_project.objects.all())
     satellite = django_filters.ModelChoiceFilter(name='space_project',
                                                  queryset = models.Space_project.objects.all())
-        
+
     class Meta:
         model = models.Session
         fields = ['space_project', 'time_begin', 'time_end', 'project', 'satellite']
@@ -64,7 +64,7 @@ class ChannelsView(viewsets.ReadOnlyModelViewSet):
     queryset = models.Channel.objects.all()
     serializer_class = serializer.ChannelsSerializer
     permission_classes = (PromisPermission,)
-    
+
 class SessionsView(viewsets.ReadOnlyModelViewSet):
     queryset = models.Session.objects.all()
     serializer_class = serializer.SessionsSerializer
@@ -79,7 +79,7 @@ class SessionsView(viewsets.ReadOnlyModelViewSet):
         polygon = self.request.query_params.get('polygon', None)
 
 #commented to allow Anonymous access
-        '''     
+        '''
         user = self.request.user
         if not helpers.UserExists(user):
             return models.Session.objects.none()
@@ -92,7 +92,7 @@ class SessionsView(viewsets.ReadOnlyModelViewSet):
         else:
             queryset = models.Session.objects.all()
         '''
-        
+
         if polygon is not None:
             try:
                 geoobj = GEOSGeometry(polygon, srid = 4326)
@@ -158,14 +158,14 @@ class QuicklookView(RetrieveModelMixin, viewsets.GenericViewSet):
         # TODO: JSON/HTML-ify the responses here?
         # TODO: 422 instead of 404 for incorrect params?
         # TODO: most of the raises here are redundant and should be done
-        # in validators. 
-        
+        # in validators.
+
         # Determining the quicklook function
         quicklook_fun = getattr(obj, src_name).quicklook
-        
+
         if not quicklook_fun:
             raise MethodNotAllowed('< no quicklook defined >')
-        
+
         # TODO: many stubs here depend on the knowledge of the JSON structure
         # which varies type to type. Making 100500 functions is unfeasible, so
         # we might want to wrap this into classes with known interface and
@@ -193,18 +193,18 @@ class QuicklookView(RetrieveModelMixin, viewsets.GenericViewSet):
         # if user_not_authenticated and npoints > max_points_for_this_json * some_coeff:
         #   raise NotAuthenticated
 
-        ser = serializer.QuickLookSerializer(obj, context = { 'func': quicklook_fun, 
+        ser = serializer.QuickLookSerializer(obj, context = { 'func': quicklook_fun,
                                                               'kwargs': { 'npoints': npoints },
                                                               'serializer': src_serializer,
                                                               'source': src_name,
                                                               'need_geo_line': False } )
         return Response(ser.data)
-        
+
     @detail_route(permission_classes = [AllowAny,])
     def channel(self, request, id):
         obj = self.queryset.get(pk = id)
         return self._quicklook(obj, "channel", serializer.ChannelsSerializer)
-    
+
     @detail_route(permission_classes = [AllowAny,])
     def parameter(self, request, id):
         obj = self.queryset.get(pk = id)
@@ -219,33 +219,45 @@ class DownloadView(viewsets.ReadOnlyModelViewSet):
     permission_classes = (AllowAny,)
     serializer_class = serializer.DownloadViewSerializer
 
+# TODO: make a common base class for this and QuicklookView
 class DownloadData(RetrieveModelMixin, viewsets.GenericViewSet):
-    queryset = models.Measurement.objects.all()
-    permission_classes = (PromisPermission,)
-    serializer_class = serializer.MeasurementsSerializer
-    
-    def _export(self, obj, src_name, src_serializer, src_doc):
-        '''Generalized export function'''
-        pass
-        
+    def _export(self, obj, src_name, src_serializer):
+        # TODO: comment this code, merge it with quicklook
+        try:
+            fmt = self.request.query_params['fmt']
+        except KeyError:
+            fmt = 'json'
 
-    @detail_route(permission_classes = [PromisPermission,])
+        # if fmt == 'json':
+        ser = serializer.JSONDataSerializer(obj, context = { 'serializer': src_serializer, 'source': src_name } )
+        # else:
+        #     quicklook_fun = getattr(obj, src_name).quicklook
+        #
+        #     if not quicklook_fun:
+        #         raise MethodNotAllowed('< no quicklook defined >')
+        #
+        #     ser = serializer.QuickLookSerializer(obj, context = { 'func': quicklook_fun,
+        #                                                           'kwargs': { 'npoints': npoints },
+        #                                                           'serializer': src_serializer,
+        #                                                           'source': src_name,
+        #                                                           'need_geo_line': False } )
+
+        return Response(ser.data)
+
+# TODO: why is it pk here and id above???
+    @detail_route(permission_classes = [AllowAny,])
     def channel(self, request, pk):
-        if pk:
-            obj = self.queryset.get(pk = pk)
-            ser = serializer.ChannelDataSerializer(obj)
-            return Response(ser.data)
-        else:
-            return Response([])
+        obj = self.queryset.get(pk = pk)
+        return self._export(obj, "channel", serializer.ChannelsSerializer)
 
-    @detail_route(permission_classes = [PromisPermission,])
+    @detail_route(permission_classes = [AllowAny,])
     def parameter(self, request, pk):
-        if pk:
-            obj = self.queryset.get(pk = pk)
-            ser = serializer.ParameterDataSerializer(obj)
-            return Response(ser.data)
-        else:
-            return Response([])
+        obj = self.queryset.get(pk = pk)
+        return self._export(obj, "parameter", serializer.ParametersSerializer)
+
+    queryset = models.Measurement.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = serializer.MeasurementsSerializer
 
 class UserPagination(LimitOffsetPagination):
     def get_paginated_response(self, data):
@@ -258,7 +270,7 @@ class UserViewSet(viewsets.GenericViewSet, CreateModelMixin, ListModelMixin):
     queryset = get_user_model().objects
     serializer_class = serializer.UserSerializer
     pagination_classes = UserPagination
-    
+
     def get_permissions(self):
         if self.request.method == 'POST':
             self.permission_classes = (AllowAny,)
@@ -272,12 +284,12 @@ class UserViewSet(viewsets.GenericViewSet, CreateModelMixin, ListModelMixin):
             return get_user_model().objects.filter(username = self.request.user)
         else:
             get_user_model().objects.none()
-            
+
     def paginate_queryset(self, queryset, view=None):
     # couldn't find a better solution
         return None
-        
-        
+
+
 @api_view(['POST', 'PUT'])
 @permission_classes((SelfProfilePermission, IsAuthenticated))
 def UserUpdate(request):
@@ -285,13 +297,12 @@ def UserUpdate(request):
         user = get_user_model().objects.get(username = request.user)
     except get_user_model().DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     if request.method == 'POST' or request.method == 'PUT':
-            
+
             ser = serializer.UserSerializer(user, data=request.data, partial=True)
             if ser.is_valid():
                 ser.save()
                 return Response(ser.data, status=status.HTTP_202_ACCEPTED)
             else:
                 return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
-            
