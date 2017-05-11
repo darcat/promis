a3247c839a71c7a04ce43368c805dae58fcbe7cc
Oleksiy Protas <elfy.ua@gmail.com>
2017-05-11 16:54:43 +0300

    LatLonAlt order for geolines
    fixes #214

--- test/data/generate.py
+++ test/data/generate.py
@@ -44,8 +44,8 @@ round_start = int(time())
 # TODO: sql injection prevention (not that it's a concern for tests, but ..)
 
 # Fake mercator considering Earth is a sphere
-def cord_conv(x,y):
-    return [ float(x),180.0/pi*(2.0*atan(exp(y*pi/180.0))-pi/2.0), 400 ]
+def cord_conv(y, x):
+    return [ 180.0/pi*(2.0*atan(exp(y*pi/180.0))-pi/2.0), float(x), 400 ]
 
 # Ensures the number doesn't exceed 180
 def clamp180(x):
@@ -57,24 +57,24 @@ def clamp180(x):
 
 # Heart shape
 def heart():
-    return [ cord_conv(-90+4*16*sin(t)**3, 3*13*cos(t)-3*5*cos(2*t)-3*2*cos(3*t)-3*cos(4*t))
+    return [ cord_conv(3*13*cos(t)-3*5*cos(2*t)-3*2*cos(3*t)-3*cos(4*t), -90+4*16*sin(t)**3)
         for t in ((i-heart_pts/2)/(heart_pts*0.165)
             for i in range(heart_pts))]
 # Circle
 def circle():
-    return [ cord_conv(90+60*cos(t), 30*sin(t))
+    return [ cord_conv(30*sin(t), 90+60*cos(t))
         for t in ((2*pi*i/peace_pts)
-            for i in range(peace_pts))]
+            for i in range(peace_pts)) ]
 
 # Vertical line [-45;45]
 def vline():
-    return [ cord_conv(90, t*35)
+    return [ cord_conv(t*35, 90)
         for t in (2*(float(i)/lines_pts-0.5)
             for i in range(lines_pts))]
 
 # Upwards tick
 def uptick():
-    return [ cord_conv(90+t*50,-5-abs(t*30))
+    return [ cord_conv(-5-abs(t*30), 90+t*50)
         for t in (2*(float(i)/lines_pts-0.5)
             for i in range(lines_pts))]
 
@@ -82,7 +82,7 @@ def uptick():
 
 # Yes I know this is not how satellites work
 def roundabout():
-    return [ [ clamp180(round_shift*t/pi), 80*sin(t), 300 + 200*cos(t) ]
+    return [ [ 80*sin(t), clamp180(round_shift*t/pi), 300 + 200*cos(t) ]
         for t in ((2*slow_factor*round_turns*pi*i/round_pts)
             for i in range(round_pts))]
 
